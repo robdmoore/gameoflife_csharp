dd5e4f90c6ccc3e8438382f9576297f7611db405
Robert Moore <me@robdmoore.id.au>
2012-01-11 00:02:32 +0800

    Added next test

--- GameOfLife2.Tests/WorldTests.cs
+++ GameOfLife2.Tests/WorldTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace GameOfLife2.Tests
@@ -7,17 +8,27 @@ namespace GameOfLife2.Tests
     class WorldShould
     {
         [Test, Combinatorial]
-        public void Return_number_of_alive_neighbours_for_a_coordinate_and_a_list_of_alive_cells([Range(-1,1)] int x, [Range(-1,1)] int y)
+        /*
+                        Y
+            0 0 0 0 0  -2
+            0 1 0 0 0  -1
+            0 0 0 0 0   0
+            0 0 0 1 0   1
+            0 0 0 0 0   2
+
+         X -2-1 0 1 2
+        */
+        public void Return_number_of_alive_neighbours_for_a_coordinate_and_a_list_of_alive_cells([Range(-2,2)] int x, [Range(-2,2)] int y)
         {
             var cell = new Coordinate(x, y);
             var aliveCells = new HashSet<Coordinate> {new Coordinate(-1, -1), new Coordinate(1, 1)};
 
-            var numNeighbours = 0;
+            var numNeighbours = cell.GetNeighbours().Count(aliveCells.Contains);
 
             var expectedNeighbours = 1;
-            if (x == 0 && x == 0)
-                expectedNeighbours = 1;
-            if ((x > 0 && y > 0) || (x < 0 && y < 0))
+            if (x == 0 && y == 0)
+                expectedNeighbours = 2;
+            if ((x > 0 && y < 0) || (x < 0 && y > 0) || aliveCells.Contains(cell))
                 expectedNeighbours = 0;
 
             Assert.That(numNeighbours, Is.EqualTo(expectedNeighbours));
