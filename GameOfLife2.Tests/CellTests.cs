a2658e9c6dfd61ff42d0244e494d6c244945cabb
Robert Moore <me@robdmoore.id.au>
2012-01-10 23:33:05 +0800

    Added first cell test

---
+++ GameOfLife2.Tests/CellTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife2.Tests
+{
+    [TestFixture]
+    class CellTests
+    {
+        [Test]
+        public void Any_live_cell_with_fewer_than_2_neighbours_will_die([Values(0,1)] int numAliveNeighbours)
+        {
+            Assert.That(GetNewState(numAliveNeighbours), Is.False);
+        }
+
+        private static bool GetNewState(int numAliveNeighbours)
+        {
+            var alive = true;
+            if (numAliveNeighbours < 2)
+                alive = false;
+            return alive;
+        }
+    }
+}
