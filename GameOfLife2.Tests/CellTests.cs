35ce377e37e5dbc5b741eab6114feb5e378897ee
Robert Moore <me@robdmoore.id.au>
2012-01-10 23:39:32 +0800

    Next test

--- GameOfLife2.Tests/CellTests.cs
+++ GameOfLife2.Tests/CellTests.cs
@@ -29,6 +29,12 @@ namespace GameOfLife2.Tests
             Assert.That(GetNewState(false, 3), Is.True);
         }
 
+        [Test]
+        public void Any_dead_cell_with_out_3_live_neighbours_stays_dead([Values(0, 1, 2, 4, 5, 6, 7, 8)] int numAliveNeighbours)
+        {
+            Assert.That(GetNewState(false, numAliveNeighbours), Is.False);
+        }
+
         private static bool GetNewState(bool currentState, int numAliveNeighbours)
         {
             var alive = currentState;
