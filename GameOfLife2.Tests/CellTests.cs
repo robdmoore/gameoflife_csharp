291cd41fcf4683c1910105b55593db59837df932
Robert Moore <me@robdmoore.id.au>
2012-01-10 23:38:07 +0800

    Next test

--- GameOfLife2.Tests/CellTests.cs
+++ GameOfLife2.Tests/CellTests.cs
@@ -6,22 +6,36 @@ namespace GameOfLife2.Tests
     class CellTests
     {
         [Test]
-        public void Any_live_cell_with_fewer_than_2_neighbours_will_die([Values(0,1)] int numAliveNeighbours)
+        public void Any_live_cell_with_fewer_than_2_live_neighbours_will_die([Values(0,1)] int numAliveNeighbours)
         {
-            Assert.That(GetNewState(numAliveNeighbours), Is.False);
+            Assert.That(GetNewState(true, numAliveNeighbours), Is.False);
         }
 
         [Test]
-        public void Any_live_cell_with_more_than_3_neighbours_will_die([Values(4, 5, 6, 7, 8)] int numAliveNeighbours)
+        public void Any_live_cell_with_more_than_3_live_neighbours_will_die([Values(4, 5, 6, 7, 8)] int numAliveNeighbours)
         {
-            Assert.That(GetNewState(numAliveNeighbours), Is.False);
+            Assert.That(GetNewState(true, numAliveNeighbours), Is.False);
         }
 
-        private static bool GetNewState(int numAliveNeighbours)
+        [Test]
+        public void Any_live_cell_with_2_or_3_live_neighbours_will_stay_alive([Values(2,3)] int numAliveNeighbours)
         {
-            var alive = true;
-            if (numAliveNeighbours < 2 || numAliveNeighbours > 3)
+            Assert.That(GetNewState(true, numAliveNeighbours), Is.True);
+        }
+
+        [Test]
+        public void Any_dead_cell_with_3_live_neighbours_will_become_alive()
+        {
+            Assert.That(GetNewState(false, 3), Is.True);
+        }
+
+        private static bool GetNewState(bool currentState, int numAliveNeighbours)
+        {
+            var alive = currentState;
+            if (currentState && (numAliveNeighbours < 2 || numAliveNeighbours > 3))
                 alive = false;
+            if (!currentState && numAliveNeighbours == 3)
+                alive = true;
             return alive;
         }
     }
