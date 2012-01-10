9b5baea8d2fa0ed7c1c682bbba71037e0ec973ea
Robert Moore <me@robdmoore.id.au>
2012-01-11 00:08:52 +0800

    Next test

--- GameOfLife2.Tests/WorldTests.cs
+++ GameOfLife2.Tests/WorldTests.cs
@@ -1,9 +1,22 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace GameOfLife2.Tests
 {
     [TestFixture]
     class WorldShould
     {
+        [Test]
+        public void Convert_set_of_alive_cells_to_new_set_of_alive_cells()
+        {
+            var aliveCells = new HashSet<Coordinate> {new Coordinate(0,2), new Coordinate(1,2), new Coordinate(2,2)};
+
+            var newAliveCells = new HashSet<Coordinate>();
+
+            Assert.That(newAliveCells, Has.Count.EqualTo(3));
+            Assert.That(newAliveCells, Has.Member(new Coordinate(2, 0)));
+            Assert.That(newAliveCells, Has.Member(new Coordinate(2, 1)));
+            Assert.That(newAliveCells, Has.Member(new Coordinate(2, 2)));
+        }
     }
 }
