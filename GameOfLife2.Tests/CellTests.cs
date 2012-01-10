d57e741ae3a95a2360d83a8f435cf8aacb285665
Robert Moore <me@robdmoore.id.au>
2012-01-10 23:42:18 +0800

    Refactored into Cell class

--- GameOfLife2.Tests/CellTests.cs
+++ GameOfLife2.Tests/CellTests.cs
@@ -8,36 +8,31 @@ namespace GameOfLife2.Tests
         [Test]
         public void Any_live_cell_with_fewer_than_2_live_neighbours_will_die([Values(0,1)] int numAliveNeighbours)
         {
-            Assert.That(GetNewState(true, numAliveNeighbours), Is.False);
+            Assert.That(Cell.GetNewState(true, numAliveNeighbours), Is.False);
         }
 
         [Test]
         public void Any_live_cell_with_more_than_3_live_neighbours_will_die([Values(4, 5, 6, 7, 8)] int numAliveNeighbours)
         {
-            Assert.That(GetNewState(true, numAliveNeighbours), Is.False);
+            Assert.That(Cell.GetNewState(true, numAliveNeighbours), Is.False);
         }
 
         [Test]
         public void Any_live_cell_with_2_or_3_live_neighbours_will_stay_alive([Values(2,3)] int numAliveNeighbours)
         {
-            Assert.That(GetNewState(true, numAliveNeighbours), Is.True);
+            Assert.That(Cell.GetNewState(true, numAliveNeighbours), Is.True);
         }
 
         [Test]
         public void Any_dead_cell_with_3_live_neighbours_will_become_alive()
         {
-            Assert.That(GetNewState(false, 3), Is.True);
+            Assert.That(Cell.GetNewState(false, 3), Is.True);
         }
 
         [Test]
         public void Any_dead_cell_with_out_3_live_neighbours_stays_dead([Values(0, 1, 2, 4, 5, 6, 7, 8)] int numAliveNeighbours)
         {
-            Assert.That(GetNewState(false, numAliveNeighbours), Is.False);
-        }
-
-        private static bool GetNewState(bool currentState, int numAliveNeighbours)
-        {
-            return numAliveNeighbours == 3 || (currentState && numAliveNeighbours == 2);
+            Assert.That(Cell.GetNewState(false, numAliveNeighbours), Is.False);
         }
     }
 }
---
+++ GameOfLife2/Cell.cs
@@ -0,0 +1,10 @@
+﻿namespace GameOfLife2
+{
+    public class Cell
+    {
+        public static bool GetNewState(bool currentState, int numAliveNeighbours)
+        {
+            return numAliveNeighbours == 3 || (currentState && numAliveNeighbours == 2);
+        }
+    }
+}
