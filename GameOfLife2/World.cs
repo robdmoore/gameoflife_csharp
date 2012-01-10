5a2cf59da060ff36e2fe7204da77510a978e6f78
Robert Moore <me@robdmoore.id.au>
2012-01-11 00:35:13 +0800

    Refactored Cell and Coordinate to be in the same file as World so the whole solution can be viewed together

--- GameOfLife2/World.cs
+++ GameOfLife2/World.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GameOfLife2
@@ -19,6 +20,52 @@ namespace GameOfLife2
                 ).ToList().ForEach(c => newAliveCells.Add(c));
             }
             return newAliveCells;
+        }
+    }
+
+    public class Cell
+    {
+        public static bool GetNewState(bool currentState, int numAliveNeighbours)
+        {
+            return numAliveNeighbours == 3 || (currentState && numAliveNeighbours == 2);
+        }
+    }
+    
+    public class Coordinate : Tuple<int, int>
+    {
+        public Coordinate(int x, int y) : base(x, y) { }
+
+        private int X { get { return Item1; } }
+        private int Y { get { return Item2; } }
+
+        public IEnumerable<Coordinate> GetNeighbours()
+        {
+            return new[]
+            {
+                new Coordinate(X-1, Y-1),
+                new Coordinate(X  , Y-1),
+                new Coordinate(X+1, Y-1),
+                new Coordinate(X+1, Y  ),
+                new Coordinate(X+1, Y+1),
+                new Coordinate(X  , Y+1),
+                new Coordinate(X-1, Y+1),
+                new Coordinate(X-1, Y  ),
+            };
+        }
+
+        public int GetAliveNeighbours(ICollection<Coordinate> aliveCells)
+        {
+            return GetNeighbours().Count(aliveCells.Contains);
+        }
+
+        public bool RemainsAlive(ISet<Coordinate> alive)
+        {
+            return Cell.GetNewState(alive.Contains(this), GetAliveNeighbours(alive));
+        }
+
+        public bool BecomesAlive(ISet<Coordinate> alive)
+        {
+            return RemainsAlive(alive);
         }
     }
 }
